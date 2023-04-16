86c2dead131c43cff742a5c30c5e9f4992457ff9
星河 <1821365384@qq.com>
2023-04-16 20:08:28 +0800

    Fix bugs.

--- Lab1/3.sql
+++ Lab1/3.sql
@@ -1,57 +1,23 @@
 DROP PROCEDURE IF EXISTS updateReaderID;
 DELIMITER //
-CREATE PROCEDURE updateReaderID(IN originID CHAR(8), IN newID CHAR(8)) BEGIN
-    DECLARE state INT DEFAULT 0;
-    DECLARE readerName VARCHAR(10);
-    DECLARE readerAge INT;
-    DECLARE readerAddress VARCHAR(20);
-    
-    DECLARE borrowedBooks CURSOR FOR
-        (SELECT book_ID, borrow_Date, return_Date FROM Borrow
-        WHERE reader_ID = originID);
-    
-    DECLARE reservedBooks CURSOR FOR
-        (SELECT book_ID, reserve_Date, take_Date FROM Reserve
-        WHERE reader_ID = originID);
-    
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET state = 1;
 
-    SELECT name, age, address FROM Reader
-    WHERE ID = originID
-    INTO readerName, readerAge, readerAddress;
-
-    INSERT INTO Reader VALUES (newID, readerName, readerAge, readerAddress);
-    
-    OPEN borrowedBooks;
-    REPEAT BEGIN
-        DECLARE bookID CHAR(8);
-        DECLARE borrowDate, returnDate DATE;
-        FETCH borrowedBooks INTO bookID, borrowDate, returnDate;
-        IF state = 0 THEN
-            INSERT INTO Borrow VALUES (bookID, newID, borrowDate, returnDate);
-        END IF;
-	END;
-    UNTIL state = 1
-    END REPEAT;
-    CLOSE borrowedBooks;
-
-    SET state = 0;
-
-    OPEN reservedBooks;
-    REPEAT BEGIN
-        DECLARE bookID CHAR(8);
-        DECLARE reserveDate, takeDate DATE;
-        FETCH reservedBooks INTO bookID, reserveDate, takeDate;
-        IF state = 0 THEN
-            INSERT INTO Borrow VALUES (bookID, newID, reserveDate, takeDate);
-        END IF;
-	END;
-    UNTIL state = 1
-    END REPEAT;
-    CLOSE reservedBooks;
-    
-    DELETE FROM Borrow WHERE reader_ID = originID;
-    DELETE FROM Reserve WHERE reader_ID = originID;
-    DELETE FROM Reader WHERE ID = originID;
+CREATE PROCEDURE updateReaderID(IN originID CHAR(8), IN newID CHAR(8),
+    OUT status VARCHAR(50)) BEGIN
+    DECLARE s INT DEFAULT 0;
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET s = 1;
+    START TRANSACTION;
+    SET FOREIGN_KEY_CHECKS = 0;
+    UPDATE Reader SET ID = newID WHERE ID = originID;
+    UPDATE Borrow SET reader_ID = newID WHERE reader_ID = originID;
+    UPDATE Reserve SET reader_ID = newID WHERE reader_ID = originID;
+    SET FOREIGN_KEY_CHECKS = 1;
+    IF s = 0 THEN
+        SET status = 'OK';
+        COMMIT;
+    ELSE 
+        SET status = 'Failed.';
+        ROLLBACK;
+    END IF;
 END //
+
 DELIMITER ;
