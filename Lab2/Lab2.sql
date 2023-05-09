4b7edeb153cde75142b2bf436173172fc22fb6cc
星河 <1821365384@qq.com>
2023-05-09 23:29:15 +0800

    修正实验2.

--- Lab2/Lab2.sql
+++ Lab2/Lab2.sql
@@ -1,8 +1,10 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2023/5/6 20:58:13                            */
+/* Created on:     2023/5/9 22:46:23                            */
 /*==============================================================*/
 
+
+drop table if exists Account;
 
 drop table if exists Ascription;
 
@@ -10,13 +12,15 @@ drop table if exists Borrow;
 
 drop table if exists Checking;
 
-drop table if exists CheckingAccount;
-
 drop table if exists Contacts;
 
 drop table if exists Department;
 
-drop table if exists LoanPay;
+drop table if exists Loan;
+
+drop table if exists OpenCheckAcc;
+
+drop table if exists PayRecord;
 
 drop table if exists Person;
 
@@ -24,9 +28,22 @@ drop table if exists Response;
 
 drop table if exists Saving;
 
-drop table if exists SavingAccount;
-
 drop table if exists Subbank;
+
+drop table if exists openSavAcc;
+
+/*==============================================================*/
+/* Table: Account                                               */
+/*==============================================================*/
+create table Account
+(
+   accID                char(20) not null,
+   balance              float(8,2) not null,
+   accDate              date not null,
+   lastAccDate          date,
+   accType              bool not null,
+   primary key (accID)
+);
 
 /*==============================================================*/
 /* Table: Ascription                                            */
@@ -47,8 +64,7 @@ create table Borrow
 (
    loanID               char(20) not null,
    ID                   char(20) not null,
-   bankName             char(20) not null,
-   primary key (loanID, ID, bankName)
+   primary key (loanID, ID)
 );
 
 /*==============================================================*/
@@ -58,21 +74,7 @@ create table Checking
 (
    accID                char(20) not null,
    overdraft            float(8,2) not null,
-   balance              float(8,2) not null,
-   accDate              date not null,
-   lastAccDate          date,
    primary key (accID)
-);
-
-/*==============================================================*/
-/* Table: CheckingAccount                                       */
-/*==============================================================*/
-create table CheckingAccount
-(
-   ID                   char(20) not null,
-   accID                char(20) not null,
-   bankName             char(20) not null,
-   primary key (ID, accID, bankName)
 );
 
 /*==============================================================*/
@@ -98,14 +100,35 @@ create table Department
 );
 
 /*==============================================================*/
-/* Table: LoanPay                                               */
+/* Table: Loan                                                  */
 /*==============================================================*/
-create table LoanPay
+create table Loan
 (
    loanID               char(20) not null,
-   amount               float(8,2) not null,
-   payDate              date not null,
+   bankName             char(20) not null,
+   totalAmount          float(8,2) not null,
    primary key (loanID)
+);
+
+/*==============================================================*/
+/* Table: OpenCheckAcc                                          */
+/*==============================================================*/
+create table OpenCheckAcc
+(
+   bankName             char(20) not null,
+   ID                   char(20) not null,
+   accID                char(20) not null,
+   primary key (bankName, ID)
+);
+
+/*==============================================================*/
+/* Table: PayRecord                                             */
+/*==============================================================*/
+create table PayRecord
+(
+   loanID               char(20) not null,
+   payDate              date not null,
+   amount               float(8,2) not null
 );
 
 /*==============================================================*/
@@ -140,21 +163,7 @@ create table Saving
    accID                char(20) not null,
    rate                 decimal not null,
    moneyType            char(20) not null,
-   balance              float(8,2) not null,
-   accDate              date not null,
-   lastAccDate          date,
    primary key (accID)
-);
-
-/*==============================================================*/
-/* Table: SavingAccount                                         */
-/*==============================================================*/
-create table SavingAccount
-(
-   ID                   char(20) not null,
-   accID                char(20) not null,
-   bankName             char(20) not null,
-   primary key (ID, accID, bankName)
 );
 
 /*==============================================================*/
@@ -167,6 +176,17 @@ create table Subbank
    primary key (bankName)
 );
 
+/*==============================================================*/
+/* Table: openSavAcc                                            */
+/*==============================================================*/
+create table openSavAcc
+(
+   ID                   char(20) not null,
+   bankName             char(20) not null,
+   accID                char(20) not null,
+   primary key (ID, bankName)
+);
+
 alter table Ascription add constraint FK_Ascription foreign key (ID)
       references Person (ID) on delete restrict on update restrict;
 
@@ -174,22 +194,13 @@ alter table Ascription add constraint FK_Ascription2 foreign key (depID)
       references Department (depID) on delete restrict on update restrict;
 
 alter table Borrow add constraint FK_Borrow foreign key (loanID)
-      references LoanPay (loanID) on delete restrict on update restrict;
+      references Loan (loanID) on delete restrict on update restrict;
 
 alter table Borrow add constraint FK_Borrow2 foreign key (ID)
       references Person (ID) on delete restrict on update restrict;
 
-alter table Borrow add constraint FK_Borrow3 foreign key (bankName)
-      references Subbank (bankName) on delete restrict on update restrict;
-
-alter table CheckingAccount add constraint FK_CheckingAccount foreign key (ID)
-      references Person (ID) on delete restrict on update restrict;
-
-alter table CheckingAccount add constraint FK_CheckingAccount2 foreign key (accID)
-      references Checking (accID) on delete restrict on update restrict;
-
-alter table CheckingAccount add constraint FK_CheckingAccount3 foreign key (bankName)
-      references Subbank (bankName) on delete restrict on update restrict;
+alter table Checking add constraint FK_type1 foreign key (accID)
+      references Account (accID) on delete restrict on update restrict;
 
 alter table Contacts add constraint FK_Contacts foreign key (Per_ID)
       references Person (ID) on delete restrict on update restrict;
@@ -200,18 +211,36 @@ alter table Contacts add constraint FK_Contacts2 foreign key (ID)
 alter table Department add constraint FK_Leaded foreign key (ID)
       references Person (ID) on delete restrict on update restrict;
 
+alter table Loan add constraint FK_���� foreign key (bankName)
+      references Subbank (bankName) on delete restrict on update restrict;
+
+alter table OpenCheckAcc add constraint FK_onlyOneCheckAccPerManPerBank foreign key (accID)
+      references Checking (accID) on delete restrict on update restrict;
+
+alter table OpenCheckAcc add constraint FK_openCheckAccInBanks foreign key (ID)
+      references Person (ID) on delete restrict on update restrict;
+
+alter table OpenCheckAcc add constraint FK_sharedCheckAcc foreign key (bankName)
+      references Subbank (bankName) on delete restrict on update restrict;
+
+alter table PayRecord add constraint FK_LoanPay foreign key (loanID)
+      references Loan (loanID) on delete restrict on update restrict;
+
 alter table Response add constraint FK_Response foreign key (ID)
       references Person (ID) on delete restrict on update restrict;
 
 alter table Response add constraint FK_Response2 foreign key (Per_ID)
       references Person (ID) on delete restrict on update restrict;
 
-alter table SavingAccount add constraint FK_SavingAccount foreign key (ID)
-      references Person (ID) on delete restrict on update restrict;
+alter table Saving add constraint FK_type2 foreign key (accID)
+      references Account (accID) on delete restrict on update restrict;
 
-alter table SavingAccount add constraint FK_SavingAccount2 foreign key (accID)
+alter table openSavAcc add constraint FK_onlyOneSavAccPerManPerBank foreign key (accID)
       references Saving (accID) on delete restrict on update restrict;
 
-alter table SavingAccount add constraint FK_SavingAccount3 foreign key (bankName)
+alter table openSavAcc add constraint FK_openSavAccInBanks foreign key (ID)
+      references Person (ID) on delete restrict on update restrict;
+
+alter table openSavAcc add constraint FK_sharedSavAcc foreign key (bankName)
       references Subbank (bankName) on delete restrict on update restrict;
 
