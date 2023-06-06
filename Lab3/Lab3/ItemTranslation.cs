6bafba80b5e07d4bd75e9455ebdf0139c0f91134
星河 <1821365384@qq.com>
2023-06-06 15:37:52 +0800

    Add login

--- Lab3/Lab3/ItemTranslation.cs
+++ Lab3/Lab3/ItemTranslation.cs
@@ -1,20 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Lab3 {
+    public class ItemTranslation {
+        public static readonly string[] Gender = {
+            "", 
+            "男",  // 1
+            "女",  // 2
+        };
 
-namespace Lab3 {
-    enum Gender : byte {
-        男 = 1,
-        女 = 2
+        public static readonly string[] StudentType = {
+            "",
+            "本科生",      // 1
+            "硕士研究生",  // 2
+            "博士研究生",  // 3
+        };
+
+        public static readonly string[] TeacherType = {
+            "",
+            "博士后",       // 1
+            "助教",         // 2
+            "讲师",         // 3 
+            "副教授",       // 4
+            "特任教授",     // 5
+            "教授",         // 6
+            "助理研究员",    // 7
+            "特任副研究员",  // 8
+            "副研究员",     // 9
+            "特任研究员",   // 10
+            "研究员",      // 11
+        };
+
+        public static readonly string[] PaperType = {
+            "",
+            "full paper",    // 1
+            "short paper",   // 2
+            "poster paper",  // 3
+            "demo paper",    // 4
+        };
+
+        public static readonly string[] PaperLevel = {
+            "",
+            "CCF-A",       // 1
+            "CCF-B",       // 2
+            "CCF-C",       // 3
+            "中文 CCF-A",  // 4
+            "中文 CCF-B",  // 5
+            "无级别",      // 6 
+        };
+
+        public static readonly string[] ProjectType = {
+            "",
+            "国家级项目",    // 1
+            "省部级项目",    // 2
+            "市厅级项目",    // 3
+            "企业合作项目",  // 4
+            "其它类型项目",  // 5
+        };
+
+        public static readonly string[] Term = {
+            "",
+            "春季学期",  // 1
+            "夏季学期",  // 2
+            "秋季学期",  // 3
+        };
     }
-
-    enum StudentType : byte {
-        本科生 = 1,
-        硕士研究生 = 2,
-        博士研究生 = 3
-    }
-
-    
 }
--- Lab3/Lab3/MainWindow.xaml.cs
+++ Lab3/Lab3/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,16 +14,37 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Lab3
-{
+namespace Lab3 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
+        }
+
+        private void CloseWindow_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+
+        private void MoveWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            DragMove();
+        }
+
+        private async Task<string> GetYiYanWords() {
+            string res;
+            try {
+                var client = new HttpClient();
+                res = await client.GetStringAsync("https://v1.hitokoto.cn/?c=k&encode=text");
+            }
+            catch (Exception) {
+                res = "我们所爱之物昭示着我们究竟是谁。";
+            }
+            return res;
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e) {
+            apothegm.Text = await GetYiYanWords();
         }
     }
 }
